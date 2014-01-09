24a3b812b2066518e807b9bfe1b158fc3d679d38
Sakshi Jain <sakshijain1107@gmail.com>
2014-01-09 18:07:25 +0530

    Update retweets_controller.rb

--- app/controllers/retweets_controller.rb
+++ app/controllers/retweets_controller.rb
@@ -9,10 +9,10 @@ class RetweetsController < ApplicationController
     # Certain methods require authentication. To get your Twitter OAuth credentials,
     # register an app at http://dev.twitter.com/apps
     Twitter.configure do |config|
-      config.consumer_key = 'KaChM1SkMvnZF7hcGMA'
-      config.consumer_secret =  '29AY0j8IR9xy14NbpEIY0Pgqf7utpGPO3s1PcxYrys'
-      config.oauth_token = '1500225216-7SxZm2NWHNjxSDNhDTTg11cIk0RiOTXhSsd4HWo'
-      config.oauth_token_secret = 'Yr5cJHIrhiFZzE8PX13hQ7TXcCbCX4hl8Gm1pkoYVhNj5'
+      config.consumer_key = ''
+      config.consumer_secret =  ''
+      config.oauth_token = ''
+      config.oauth_token_secret = ''
     end
   
     # Initialize your Twitter client
